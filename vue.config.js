07a6bfa65df16248d95d29d2efe8c99a11c88323
zlx <1114987152@qq.com>
2021-08-09 22:46:15 +0800

    初始化

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+// const path = require('path');//引入path模块
+// function resolve(dir){
+//     return path.join(__dirname,dir)//path.join(__dirname)设置绝对路径
+// }
+// module.exports={
+//     chainWebpack:(config)=>{
+//         config.resolve.alias
+//         .set('@',resolve('./src'));
+//     }
+// }
